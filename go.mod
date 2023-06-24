84f2ed186661d3f5ce0c2c6676431dbf28770671
om <pleasedontcry@pm.me>
2023-06-24 04:50:54 +0200

    feat: video compressor implemented

---
+++ go.mod
@@ -0,0 +1,19 @@
+module do-functions
+
+go 1.18
+
+require (
+	github.com/aws/aws-sdk-go v1.44.287 // indirect
+	github.com/gofrs/uuid v4.4.0+incompatible // indirect
+	github.com/graph-gophers/graphql-go v1.5.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/memphisdev/memphis.go v1.0.2 // indirect
+	github.com/nats-io/nats.go v1.25.0 // indirect
+	github.com/nats-io/nkeys v0.4.4 // indirect
+	github.com/nats-io/nuid v1.0.1 // indirect
+	github.com/santhosh-tekuri/jsonschema/v5 v5.1.0 // indirect
+	golang.org/x/crypto v0.6.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+)
---
+++ video-compressor.go
@@ -0,0 +1,291 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/joho/godotenv"
+	"github.com/memphisdev/memphis.go"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"strings"
+	"sync"
+)
+
+type createS3InstanceParams struct {
+	secret string
+	keyId  string
+}
+
+type downloadFromS3Params struct {
+	fileName string
+	bucket   string
+	svc      *s3.S3
+}
+
+func getVideoThumbnail(videoPath string, outputPath string) error {
+	cmd := exec.Command("ffmpeg", "-i", videoPath, "-ss", "00:00:03", "-vframes", "1", outputPath)
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func createThumbnail(fileName string) (string, error) {
+	thumbnailName := fileName[:len(fileName)-4] + "_thumbnail.jpg"
+
+	thumbnailErr := getVideoThumbnail(fileName, thumbnailName)
+
+	if thumbnailErr != nil {
+		return "", thumbnailErr
+	}
+
+	return thumbnailName, nil
+}
+
+func compressVideo(inputPath string, outputPath string) error {
+	cmd := exec.Command("ffmpeg", "-i", inputPath, "-vf", "scale=1920:1080", outputPath)
+	err := cmd.Run()
+	if err != nil {
+		return fmt.Errorf("failed to compress video: %w", err)
+	}
+
+	return nil
+}
+
+func createS3Instance(params createS3InstanceParams) (*s3.S3, error) {
+	sess, err := session.NewSession(&aws.Config{
+		Endpoint:    aws.String("ams3.digitaloceanspaces.com"),
+		Region:      aws.String("us-east-1"),
+		Credentials: credentials.NewStaticCredentials(params.keyId, params.secret, "")},
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return s3.New(sess), nil
+}
+
+func downloadFromS3(params downloadFromS3Params) error {
+	file, err := os.Create(params.fileName)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	resp, err := params.svc.GetObject(&s3.GetObjectInput{
+		Bucket: aws.String(params.bucket),
+		Key:    aws.String(params.fileName),
+	})
+	if err != nil {
+		return err
+	}
+
+	_, err = file.ReadFrom(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func uploadToS3(fileName, bucket string, svc *s3.S3) error {
+	file, err := os.Open(fileName)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	params := &s3.PutObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(fileName),
+		Body:   file,
+	}
+
+	_, err = svc.PutObject(params)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func addFullHD(str string) string {
+	dotIndex := strings.LastIndex(str, ".")
+	if dotIndex == -1 {
+		return str + "-full-hd"
+	}
+	return str[:dotIndex] + "-full-hd" + str[dotIndex:]
+}
+
+type message struct {
+	CompressedFileName string `json:"compressedFileName"`
+	ThumbnailName      string `json:"thumbnailName"`
+	OriginalFileName   string `json:"originalFileName"`
+}
+
+type tokenResponse struct {
+	Jwt                   string `json:"jwt"`
+	ExpiresIn             int    `json:"expires_in"`
+	JwtRefreshToken       string `json:"jwt_refresh_token"`
+	RefreshTokenExpiresIn int    `json:"refresh_token_expires_in"`
+}
+
+func produceMessage(messageParam message) {
+	conn, err := memphis.Connect("localhost", "root", memphis.Password("memphis"))
+	if err != nil {
+		os.Exit(1)
+	}
+	defer conn.Close()
+	p, err := conn.CreateProducer("video-compression", "file-compressor")
+
+	hdrs := memphis.Headers{}
+	hdrs.New()
+	err = hdrs.Add("key", "value")
+
+	if err != nil {
+		fmt.Printf("Header failed: %v", err)
+		os.Exit(1)
+	}
+
+	jsonData, err := json.Marshal(messageParam)
+	if err != nil {
+		errorHandler(err)
+	}
+
+	err = p.Produce(jsonData, memphis.MsgHeaders(hdrs))
+
+	if err != nil {
+		fmt.Printf("Produce failed: %v", err)
+		os.Exit(1)
+	}
+}
+
+func errorHandler(err error) {
+	log.Fatal(err)
+	os.Exit(1)
+}
+
+func getToken(sessionToken chan string) {
+	url := os.Getenv("MEMPHIS_HOST") + "/auth/authenticate"
+	method := "POST"
+
+	payload := strings.NewReader(fmt.Sprintf(`{
+		"username": "%s",
+		"password": "%s",
+		"token_expiry_in_minutes": 123,
+		"refresh_token_expiry_in_minutes": 10000092
+	}`, os.Getenv("MEMPHIS_USERNAME"), os.Getenv("MEMPHIS_PASSWORD")))
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, payload)
+
+	if err != nil {
+		errorHandler(err)
+	}
+	req.Header.Add("Content-Type", "application/json")
+
+	res, err := client.Do(req)
+	if err != nil {
+		errorHandler(err)
+	}
+	defer res.Body.Close()
+
+	data := new(tokenResponse)
+	json.NewDecoder(res.Body).Decode(data)
+
+	if err != nil {
+		errorHandler(err)
+	}
+
+	sessionToken <- data.Jwt
+}
+
+func handleVideo(fileName, bucket string, svc *s3.S3, result chan string) {
+	compressedVideoName := addFullHD(fileName)
+
+	videoErr := compressVideo(fileName, compressedVideoName)
+	if videoErr != nil {
+		errorHandler(videoErr)
+	}
+
+	videoUploadErr := uploadToS3(compressedVideoName, bucket, svc)
+	if videoUploadErr != nil {
+		errorHandler(videoUploadErr)
+	}
+
+	result <- compressedVideoName
+}
+
+func handleThumbnail(fileName, bucket string, svc *s3.S3, result chan string) {
+	thumbnail, thumbnailErr := createThumbnail(fileName)
+	if thumbnailErr != nil {
+		errorHandler(thumbnailErr)
+	}
+
+	thumbnailUploadErr := uploadToS3(thumbnail, bucket, svc)
+	if thumbnailUploadErr != nil {
+		errorHandler(thumbnailUploadErr)
+	}
+
+	result <- thumbnail
+}
+
+func main() {
+	godotenv.Load()
+	bucketSecret := os.Getenv("AWS_SECRET_ACCESS_KEY")
+	bucketSecretId := os.Getenv("AWS_ACCESS_KEY")
+	compressedVideoName := make(chan string)
+	thumbnail := make(chan string)
+
+	// temp
+	fileName := "video.mp4"
+	tempBucket := "pullapptemp"
+	bucket := "pullappspaces"
+
+	var wg sync.WaitGroup
+
+	wg.Add(2)
+
+	svc, s3Error := createS3Instance(createS3InstanceParams{
+		secret: bucketSecret,
+		keyId:  bucketSecretId,
+	})
+	if s3Error != nil {
+		errorHandler(s3Error)
+	}
+
+	err := downloadFromS3(downloadFromS3Params{
+		fileName: fileName,
+		bucket:   tempBucket,
+		svc:      svc,
+	})
+	if err != nil {
+		errorHandler(err)
+	}
+
+	go func() {
+		defer wg.Done()
+		handleVideo(fileName, bucket, svc, compressedVideoName)
+	}()
+
+	go func() {
+		defer wg.Done()
+		handleThumbnail(fileName, bucket, svc, thumbnail)
+	}()
+
+	defer wg.Wait()
+
+	produceMessage(message{
+		CompressedFileName: <-compressedVideoName,
+		ThumbnailName:      <-thumbnail,
+		OriginalFileName:   fileName,
+	})
+}
