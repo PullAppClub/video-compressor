90105355c63569f3a1bc5a2f458b13ed42b93ffc
om <pleasedontcry@pm.me>
2023-06-28 23:40:32 +0200

    temp

--- packages/video/video-compressor/video-compressor.go
+++ packages/video/video-compressor/video-compressor.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/joho/godotenv"
+	"github.com/memphisdev/memphis.go"
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 type downloadFromS3Params struct {
@@ -20,29 +24,6 @@ type downloadFromS3Params struct {
 type createS3InstanceParams struct {
 	secret string
 	keyId  string
-}
-
-func downloadFromS3(params downloadFromS3Params) error {
-	file, err := os.Create(params.fileName)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	resp, err := params.svc.GetObject(&s3.GetObjectInput{
-		Bucket: aws.String(params.bucket),
-		Key:    aws.String(params.fileName),
-	})
-	if err != nil {
-		return err
-	}
-
-	_, err = file.ReadFrom(resp.Body)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
 
 func createS3Instance(params createS3InstanceParams) (*s3.S3, error) {
@@ -94,18 +75,6 @@ func getVideoThumbnail(videoPath string, outputPath string) error {
 	return nil
 }
 
-func createThumbnail(fileName string) (string, error) {
-	thumbnailName := fileName[:len(fileName)-4] + "_thumbnail.jpg"
-
-	thumbnailErr := getVideoThumbnail(fileName, thumbnailName)
-
-	if thumbnailErr != nil {
-		return "", thumbnailErr
-	}
-
-	return thumbnailName, nil
-}
-
 func handleThumbnail(fileName, bucket string, svc *s3.S3) (thumbnail string, err error) {
 	thumbnail, err = createThumbnail(fileName)
 	if err != nil {
@@ -120,11 +89,125 @@ func handleThumbnail(fileName, bucket string, svc *s3.S3) (thumbnail string, err
 	return
 }
 
+type Request struct {
+	Name string `json:"name"`
+}
+
+type Response struct {
+	StatusCode int               `json:"statusCode,omitempty"`
+	Headers    map[string]string `json:"headers,omitempty"`
+	Body       string            `json:"body,omitempty"`
+}
+
+func createThumbnail(fileName string) (string, error) {
+	thumbnailName := fileName[:len(fileName)-4] + "_thumbnail.jpg"
+
+	thumbnailErr := getVideoThumbnail(fileName, thumbnailName)
+
+	if thumbnailErr != nil {
+		return "", thumbnailErr
+	}
+
+	return thumbnailName, nil
+}
+
+func compressVideo(inputPath string, outputPath string) error {
+	cmd := exec.Command("ffmpeg", "-i", inputPath, "-vf", "scale=1920:1080", outputPath)
+	err := cmd.Run()
+	if err != nil {
+		return fmt.Errorf("failed to compress video: %w", err)
+	}
+
+	return nil
+}
+
+func downloadFromS3(params downloadFromS3Params) error {
+	file, err := os.Create(params.fileName)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	resp, err := params.svc.GetObject(&s3.GetObjectInput{
+		Bucket: aws.String(params.bucket),
+		Key:    aws.String(params.fileName),
+	})
+	if err != nil {
+		return err
+	}
+
+	_, err = file.ReadFrom(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func addFullHD(str string) string {
+	dotIndex := strings.LastIndex(str, ".")
+	if dotIndex == -1 {
+		return str + "-full-hd"
+	}
+	return str[:dotIndex] + "-full-hd" + str[dotIndex:]
+}
+
+type message struct {
+	CompressedFileName string `json:"compressedFileName"`
+	ThumbnailName      string `json:"thumbnailName"`
+	OriginalFileName   string `json:"originalFileName"`
+}
+
+func produceMessage(messageParam message) {
+	conn, err := memphis.Connect("localhost", "root", memphis.Password("memphis"))
+	if err != nil {
+		os.Exit(1)
+	}
+	defer conn.Close()
+	p, err := conn.CreateProducer("video-compression", "file-compressor")
+
+	hdrs := memphis.Headers{}
+	hdrs.New()
+	err = hdrs.Add("key", "value")
+
+	if err != nil {
+		fmt.Printf("Header failed: %v", err)
+		os.Exit(1)
+	}
+
+	jsonData, err := json.Marshal(messageParam)
+	if err != nil {
+		errorHandler(err)
+	}
+
+	err = p.Produce(jsonData, memphis.MsgHeaders(hdrs))
+
+	if err != nil {
+		fmt.Printf("Produce failed: %v", err)
+		os.Exit(1)
+	}
+}
+
+func handleVideoCompressing(fileName, bucket string, svc *s3.S3) string {
+	compressedVideoName := addFullHD(fileName)
+
+	videoErr := compressVideo(fileName, compressedVideoName)
+	if videoErr != nil {
+		errorHandler(videoErr)
+	}
+
+	videoUploadErr := uploadToS3(compressedVideoName, bucket, svc)
+	if videoUploadErr != nil {
+		errorHandler(videoUploadErr)
+	}
+
+	return compressedVideoName
+}
+
 func Main(args map[string]interface{}) map[string]interface{} {
 	godotenv.Load()
 	bucketSecret := os.Getenv("AWS_SECRET_ACCESS_KEY")
 	bucketSecretId := os.Getenv("AWS_ACCESS_KEY")
-	//compressedVideoName := make(chan string)
 
 	// temp
 	fileName := "video.mp4"
@@ -148,18 +231,15 @@ func Main(args map[string]interface{}) map[string]interface{} {
 		errorHandler(err)
 	}
 
-	//go func() {
-	//	defer wg.Done()
-	//	handleVideo(fileName, bucket, svc, compressedVideoName)
-	//}()
-	//
-	handleThumbnail(fileName, bucket, svc)
+	compressedVideoName := handleVideoCompressing(fileName, bucket, svc)
 
-	//produceMessage(message{
-	//	CompressedFileName: <-compressedVideoName,
-	//	ThumbnailName:      <-thumbnail,
-	//	OriginalFileName:   fileName,
-	//})
+	thumbnail, _ := handleThumbnail(fileName, bucket, svc)
+
+	produceMessage(message{
+		CompressedFileName: compressedVideoName,
+		ThumbnailName:      thumbnail,
+		OriginalFileName:   fileName,
+	})
 
 	msg := make(map[string]interface{})
 	msg["body"] = "video compressed"
